e034cd831f7165f733dae1783f84c6ab98c09dcf
Michiel Decock <michieldecock@outlook.com>
2023-12-05 08:52:10 +0100

    Calculate the calibration sum

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+
+#include "core_trebuchet_calibration.h"
+
 int main(int argc, char** argv)
 {
+    const auto   lines = core::readInput("res/calibration_input");
+    const size_t sum = core::sum(core::extractNumbers(lines));
+
+    std::cout << "The calibration sum is " << sum << std::endl;
+
     return 0;
 }
